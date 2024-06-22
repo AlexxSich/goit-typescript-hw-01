6af5cacdcdbaaa4d21c9c4c19afae49333aacde6
AlexxSich <alexxsich@gmail.com>
2024-06-22 11:04:44 -0300

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,7 +1,3 @@
-//Вітаю. Підкажіть будь ласка, чому тут виникає помилка?
-// ШІ підказує, що треба спреднути ці обєкти, щоб обєднати в новий обєкт return { ...objA, ...objB }
-// а в конспекті так як написано нижче.
-
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
